26904639d62f20339606e5c4f30ee11c6a758982
Ehsan <xyxyxyxy16@gmail.com>
2024-06-12 12:35:49 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const axios = require("axios");
+const { SocksProxyAgent } = require("socks-proxy-agent");
+//proxy address if socks5 replace socks5
+const proxy = "socks://127.0.0.1:2080";
+const agent = new SocksProxyAgent(proxy);
+//gemini Api
+const token = "AIzaSyBDIUkG2wSJDU01eqHV36UGIDCZrp7h_XA";
+
+//API URL
+const apiUrl =
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" +
+  token;
+token;
+
+// send data response
+const data = {
+  contents: [
+    {
+      parts: [
+        {
+          text: "Tell me a story that really happened in the world ",
+        },
+      ],
+    },
+  ],
+};
+
+//send request by proxy
+axios
+  .post(apiUrl, data, {
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Type": "UTF-8",
+    },
+    //add proxy https
+    httpsAgent: agent,
+  })
+  .then((response) => {
+    console.log(
+      "Response from API:",
+      response.data.candidates[0].content.parts[0].text
+    );
+  })
+  .catch((error) => {
+    console.error("Error connecting to API:", error.response);
+  });
